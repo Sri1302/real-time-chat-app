01505df67fdc3ad5a9d1d1f1d466adf4dbf4def8
Sri Venkat Chennu <chennusri13@gmail.com>
2024-10-12 21:05:34 +0530

    delployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.routes.js'
@@ -11,12 +12,21 @@ import { app,server } from './socket/socket.js'
 dotenv.config()
 
 
+const __dirname = path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
+
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+})
 
 mongoose.connect(process.env.MONGODB_URL)
 .then(()=>{
